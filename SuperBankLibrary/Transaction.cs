e2a7ce3653ef0061371752cc07bfaedc209c7cb3
Ailun Shen <newhoopsdynasty@gmail.com>
2020-02-15 23:34:19 -0500

    Add transactions

---
+++ SuperBankLibrary/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperBankLibrary
+{
+    public class Transaction
+    {
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Notes { get; set; }
+        
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+    }
+}
